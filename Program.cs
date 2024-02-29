50ff8aa65a265f6fd7130b8f7576b90a575f6052
yousef mouniri <mouniriyousef98@gmail.com>
2024-02-29 08:32:43 +0100

    feat: ho aggiunto un for nel main che chiede in input i voti e pesi, e ho iniziato la prima funzione

--- Program.cs
+++ Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Inserire il numero di voti");
+            int dim = Convert.ToInt32(Console.ReadLine());
+
+            int[] voti = new int[dim];//array per i voti del primo for
+
+            int[] pesi = new int[dim];
+
+            for (int i = 0; i < dim; i++)
+            {
+                Console.WriteLine($"Inserire il {i + 1} voto");
+                voti[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Inserire il peso del {i+1} voto");
+                pesi[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+        }
+        static void StampaVotiPesi(double[] voti, int[] pesi, int nVoti)
+        {
+            for (int i = 0;i < voti.Length;i++)
+            {
+
+            }
         }
     }
 }
