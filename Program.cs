c28077aeeb916e10aff5b878162e7b70e3196c05
yousef mouniri <mouniriyousef98@gmail.com>
2024-02-29 08:45:58 +0100

    feat: ho aggiunto la seconda funzione

--- Program.cs
+++ Program.cs
@@ -18,7 +18,12 @@
                 Console.WriteLine($"Inserire il peso del {i + 1} voto");
                 pesi[i] = Convert.ToInt32(Console.ReadLine());
             }
-            StampaVotiPesi(voti, pesi, nVoti); //richiamo la prima funzione per stampare i voti e pesi inseriti dall'utente 
+            StampaVotiPesi(voti, pesi, nVoti); //richiamo la prima funzione per stampare i voti e pesi inseriti dall'utente
+            Console.WriteLine("Adesso verranno caricati dei voti e pesi casuali");
+            CaricaVettori(ref voti, ref pesi, nVoti); //richiamo la funzione, adesso i valori di voti[] e pesi[] sono cambiati
+            StampaVotiPesi(voti, pesi, nVoti);//stampo i voti e pesi generati casualmente
+
+
 
         }
         static void StampaVotiPesi(double[] voti, int[] pesi, int nVoti)
@@ -27,6 +32,17 @@
             for (int i = 0; i < voti.Length; i++)
             {
                 Console.WriteLine(voti[i] + "    " + pesi[i]);//stampa i voti e pesi alla distanza di 4 spazi
+            }
+        }
+
+        static void CaricaVettori(ref double[] voti, ref int[] pesi, int nVoti)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < nVoti; i++)
+            {
+                voti[i] = rnd.Next(0, 11);
+                pesi[i] = rnd.Next(0, 101);
+
             }
         }
     }
