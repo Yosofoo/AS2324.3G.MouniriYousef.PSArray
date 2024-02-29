f061d81707865ddb67f09bacbe23138cf4a9fabe
yousef mouniri <mouniriyousef98@gmail.com>
2024-02-29 08:39:39 +0100

    feat: ho finito la prima funzione e l'ho richiamata nel main

--- Program.cs
+++ Program.cs
@@ -5,26 +5,28 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Inserire il numero di voti");
-            int dim = Convert.ToInt32(Console.ReadLine());
+            int nVoti = Convert.ToInt32(Console.ReadLine());
 
-            int[] voti = new int[dim];//array per i voti del primo for
+            double[] voti = new double[nVoti];//array per i voti del primo for
 
-            int[] pesi = new int[dim];
+            int[] pesi = new int[nVoti];
 
-            for (int i = 0; i < dim; i++)
+            for (int i = 0; i < nVoti; i++)
             {
                 Console.WriteLine($"Inserire il {i + 1} voto");
                 voti[i] = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine($"Inserire il peso del {i+1} voto");
+                Console.WriteLine($"Inserire il peso del {i + 1} voto");
                 pesi[i] = Convert.ToInt32(Console.ReadLine());
             }
+            StampaVotiPesi(voti, pesi, nVoti); //richiamo la prima funzione per stampare i voti e pesi inseriti dall'utente 
 
         }
         static void StampaVotiPesi(double[] voti, int[] pesi, int nVoti)
         {
-            for (int i = 0;i < voti.Length;i++)
+            Console.WriteLine("Voti  " + "  Pesi");
+            for (int i = 0; i < voti.Length; i++)
             {
-
+                Console.WriteLine(voti[i] + "    " + pesi[i]);//stampa i voti e pesi alla distanza di 4 spazi
             }
         }
     }
